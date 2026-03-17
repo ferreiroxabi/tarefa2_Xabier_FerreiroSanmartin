34b35864e03b56c85b77a07968131ffe01eeb99c
XabierF <sceptileen@gmail.com>
2026-03-17 20:33:30 +0100

    Código finalizado

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ usuarios = {
     for i in range(1,16)
 }
 
-for i in usuarios:
-    print(usuarios(i))
+# Bucle para imprimir por pantalla os datos dos alumnos formateados para maior lexibilidade 
+for codigo, datos in usuarios.items():
+    print(f"Usuario {codigo}")
+    print(f"Nome: {datos['nome']}")
+    print(f"Dirección: {datos['direccion']}")
+    print(f"Correo: {datos['correo']}")
+    print(f"Teléfono: {datos['telefono']}\n")
 
+usuarioRandom = random.choice(list(usuarios.values()))
+print("O usuario chamado", usuarioRandom['nome'], "foi o afortunado")
